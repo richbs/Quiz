f8e1619309518a80535d3f751cb6645936c7f028
rbarrettsmall <webmaster@vam.ac.uk>
2011-08-08 11:02:31 +0100

    Initial Commit

---
+++ Quiz/QuizAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  QuizAppDelegate.h
+//  Quiz
+//
+//  Created by Rich Barrett-Small on 08/08/2011.
+//  Copyright 2011 Victoria & Albert Museum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
